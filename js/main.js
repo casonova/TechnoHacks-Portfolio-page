9b946875f555f6b93c04fde70ba49cc0b31e35e1
casonova <casonova.itx.193@gmail.com>
2023-09-23 02:17:16 +0500

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".typing", {
+        strings: [
+            '', 
+            "Web Designer", 
+            "Web Developer", 
+            "Python Programer",
+            "AI Research Engineer",
+            "Web Designer", 
+            "Web Developer", 
+            "Python Programer",
+            "AI Research Engineer",   
+            ''
+        ],
+        typeSpeed: 150,  
+        backSpeed: 60,   
+        startDelay: 100,
+        backDelay: 200,
+        loop: true
+    });
+});
+
